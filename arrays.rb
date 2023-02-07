ba7983dfe4436809778ae120d2cbea0fd734c864
Lauren Golden <llgolden@gmail.com>
2023-02-07 13:04:06 -0700

    Add called elements

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,16 @@
 pet_names = ["Chocolate", "Riley", "Stiles", "Jolene"]
-age = [10, 15, 5, 1]
-temperature = [98.6, 100.0, 104.2, 97.4]
+ages = [10, 15, 5, 1]
+temperatures = [98.6, 100.0, 104.2, 97.4]
 here = [false, false, true, true]
 
+#Current pet's name:
+puts pet_names[3]
+
+#Oldest pet:
+puts ages[1]
+
+#Average human temperature: 
+puts temperatures[0]
+
+#Currently here? 
+puts here[2]
